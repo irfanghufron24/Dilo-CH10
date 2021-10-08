f20716b8b87f81a0e24f65bd92e6d60c69fe4b76
irfanghufron24 <irfanghufron240@gmail.com>
2021-10-08 16:33:41 +0700

    Problem-5

---
+++ Assets/Script/MouseControll.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseControll : MonoBehaviour
+{
+    public float moveSpeed;
+    public float moveConstraint = 5.5f;
+    private Vector3 targetMove;
+
+    private Rigidbody2D p_rigidbody;
+
+    private void Awake()
+    {
+        p_rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            if (mousePos.x < moveConstraint && mousePos.x > -moveConstraint
+                && mousePos.y < moveConstraint && mousePos.y > -moveConstraint)
+            {
+                targetMove = mousePos;
+                targetMove.z = 0;
+            }
+        }
+
+        if (transform.position != targetMove)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetMove, moveSpeed * Time.deltaTime);
+        }
+    }
+}
